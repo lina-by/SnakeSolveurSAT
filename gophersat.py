4134d9fa18197b69d677b40c7bc853e776e1f57c
Nathan Janiec <nathan.janiec@student-cs.fr>
2025-02-10 16:04:24 +0100

    MVP1

--- gophersat.py
+++ gophersat.py
@@ -1,1 +1,17 @@
-# TODO: functions that translate the list of constaints into a cnf file and interpret the gophersat output 
+# TODO: functions that translate the list of constaints into a cnf file and interpret the gophersat output
+import os
+
+
+def create_sat(N_variables, clauses):
+    with open("./table.cnf", "w") as f:
+        f.write(f"p cnf {N_variables} {len(clauses)}\n")
+
+        for clause in clauses:
+            f.write(clause)
+
+    result = os.popen("gophersat table.cnf").read()
+    if result.split()[-1] == "UNSATISFIABLE":
+        return []
+
+    result = result.split()
+    return [int(x) for x in result[-N_variables - 1 : -1]]
