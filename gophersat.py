fcf1e97da6b77a13969293a7305de73b5254d59d
Nathan Janiec <nathan.janiec@student-cs.fr>
2025-02-12 16:44:13 +0100

    avancee sur le MVP2

--- gophersat.py
+++ gophersat.py
@@ -3,17 +3,31 @@ import os
 from itertools import combinations, product
 from typing import Tuple, Callable
 
-def create_variable_index(N: int, T: int, nb_pommes: int = 0) -> Callable:
-    index = {(x, y, t): N**2 * t + N * y + x + 1 for x, y, t in product(range(N), range(N), range(T))}
-    
-    for i in range(nb_pommes):
-        for t in range(T):
-            index[(N**2 * T, i, t)] = N**2*T + i*T+t + 1
 
-    def variable(id, typ: str = 's'):
-        if typ == 's':
-            if not (isinstance(id, tuple) and len(id) == 3 and all(isinstance(i, int) for i in id)):
-                raise TypeError("Wrong id format for snake position. Expected a tuple (int, int, int).")
+def create_variable_index(N: int, T: int, nb_pommes: int = 0) -> Callable:
+    index_position = {
+        (x, y, t): N**2 * t + N * y + x + 1
+        for x, y, t in product(range(N), range(N), range(T))
+    }
+    index_pommes = {
+        (p, t): N**2 * T + p * T + t + 1 for p in range(nb_pommes) for t in range(T)
+    }
+    index_counter = {
+        (c, t): N**2 * T + nb_pommes * T + c * T + t + 1
+        for c in range(nb_pommes)
+        for t in range(T)
+    }
+
+    def variable(id, typ: str = "s"):
+        if typ == "s":
+            if not (
+                isinstance(id, tuple)
+                and len(id) == 3
+                and all(isinstance(i, int) for i in id)
+            ):
+                raise TypeError(
+                    "Wrong id format for snake position. Expected a tuple (int, int, int)."
+                )
             x, y, t = id
             if x >= N:
                 raise ValueError("x is above N")
@@ -21,30 +35,43 @@ def create_variable_index(N: int, T: int, nb_pommes: int = 0) -> Callable:
                 raise ValueError("y is above N")
             if t >= T:
                 raise ValueError("t is above T")
-            return index[id]
+            return index_position[id]
 
-        if typ == 'p':
-            if not isinstance(id, tuple) or len(id)!=2:
-                raise TypeError("Wrong id format for apple. Expected an tuple of len 2 .")
+        if typ == "p":
+            if not isinstance(id, tuple) or len(id) != 2:
+                raise TypeError(
+                    "Wrong id format for apple. Expected an tuple of len 2 ."
+                )
             if id[0] >= nb_pommes:
                 raise ValueError("Apple index is over the limit")
             if id[1] >= T:
                 raise ValueError("Time index is over the limit")
-            return index[(N**2*T, id[0], id[1])]
-        
-        raise ValueError("Invalid type. 'typ' must be 's' for snake or 'p' for apple.")
+            return index_pommes[(id[0], id[1])]
+
+        if typ == "c":
+            if not isinstance(id, tuple) or len(id) != 2:
+                raise TypeError(
+                    "Wrong id format for counter. Expected an tuple of len 2 ."
+                )
+            if id[0] >= nb_pommes:
+                raise ValueError("Counter index is over the limit")
+            if id[1] >= T:
+                raise ValueError("Time index is over the limit")
+            return index_counter[(id[0], id[1])]
+
+        raise ValueError(
+            "Invalid type. 'typ' must be 's' for snake, 'p' for apple or 'c' for counter."
+        )
 
     return variable
 
 
-
-def format_clause(litteraux:list[int]):
-    clause=""
+def format_clause(litteraux: list[int]):
+    clause = ""
     for lit in litteraux:
         clause += f"{lit} "
-    clause +="0\n"
+    clause += "0\n"
     return clause
-
 
 
 def create_sat(N_variables, clauses):
